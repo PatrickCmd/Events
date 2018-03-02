d59e360f7d51881502b3cf367459a6ea3db029ab
PatrickCmd <pwalukagga@gmail.com>
2018-03-02 16:20:54 +0300

    [Feature #155390365] Refactor to follow pep8 style

--- app/auth/models.py
+++ app/auth/models.py
@@ -5,10 +5,13 @@ from app import db
 
 
 class User(db.Model):
+    '''Class to represent user table'''
+
     GENDER_TYPES = [
         ('female', 'Female'),
         ('male', 'Male')
     ]
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, index=True)
     password = db.Column(db.String(200))
@@ -17,9 +20,23 @@ class User(db.Model):
     image = db.Column(db.LargeBinary, nullable=True)
     location = db.Column(db.String(100))
     gender = db.Column(db.String(100))
-    date_of_birth = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
-    date_created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
-    date_modified = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    date_of_birth = db.Column(
+        db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    date_created = db.Column(
+        db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    date_modified = db.Column(
+        db.DateTime, nullable=False, default=datetime.datetime.utcnow)
 
-    def __str__(self):
-        return self.email
+    def __repr__(self):
+        '''Returns readable representation of the object'''
+        return "User: {}" .format(self.email)
+
+    def save(self):
+        '''save created user to the database'''
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        '''delete existing user from the database'''
+        db.session.delete(self)
+        db.session.commit()
