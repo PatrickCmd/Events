fda15a837751a899885099d99315fb4489e1d1bc
PatrickCmd <pwalukagga@gmail.com>
2018-03-02 16:29:24 +0300

    [Feature #155390365] user entries required on creation

---
+++ app/auth/args.py
@@ -0,0 +1,12 @@
+from webargs import fields, validate
+
+user_reg_args = {
+    # Reguired arguments and validations
+    'email': fields.Str(required=True),
+    'password': fields.Str(required=True, validate=validate.Length(6)),
+    'first_name': fields.Str(required=True, validate=validate.Length(3)),
+    'last_name': fields.Str(required=True, validate=validate.Length(3)),
+    'location': fields.Str(required=True),
+    'gender': fields.Str(required=True),
+    'date_of_birth': fields.Str()
+}
