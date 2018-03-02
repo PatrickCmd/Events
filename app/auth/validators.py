de6bc1fb50be916118a7a9976d0e94b6ae3f827e
PatrickCmd <pwalukagga@gmail.com>
2018-03-02 16:30:47 +0300

    [Feature #155390365] data validators

---
+++ app/auth/validators.py
@@ -0,0 +1,22 @@
+# method to check for special characters and validate a name
+def is_valid(name_string):
+    special_character = "~!@#$%^&*()_={}|\[]<>?/,;:."
+    return any(char in special_character for char in name_string)
+
+
+# check if field contains number
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
+
+# check if names contain numbers
+def name_has_numbers(data):
+    keys = ('first_name', 'last_name')
+    for key in keys:
+        if hasNumbers(data[key]):
+            return True
+
+
+# string string to remove white spaces
+def strip_clean(string):
+    return string.strip()
