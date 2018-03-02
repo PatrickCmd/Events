0aeb51a7c2fffc5d6bbf6c6a161a2d039c8fac56
PatrickCmd <pwalukagga@gmail.com>
2018-03-02 16:24:54 +0300

    [Feature #155390365] Add bcrypt and jwt managers

--- app/__init__.py
+++ app/__init__.py
@@ -6,16 +6,22 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
-from app.auth import auth
 from app.config import app_config
-from app.events import events
 
 app = Flask(__name__)
 config_name = os.environ.get('ENVIRONMENT_SETTINGS', 'development')
 app.config.from_object(app_config.get(config_name))
 
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
 db = SQLAlchemy(app)
+
+from app.auth import auth
+from app.events import events
 
 # register application blueprints
 app.register_blueprint(auth)
