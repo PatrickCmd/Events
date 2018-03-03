0846a4ded44bda9382a716db32c0ba046735b342
PatrickCmd <pwalukagga@gmail.com>
2018-03-03 11:26:45 +0300

    [Feature #155390368] Add login endpoint

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -4,9 +4,10 @@ Auth application
 
 from flask import Blueprint
 from flask_restful import Api
-from app.auth.views import UserRegistrationView
+from app.auth.views import UserRegistrationView, UserLoginView
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 auth_api = Api(auth)
 
 auth_api.add_resource(UserRegistrationView, '/register')
+auth_api.add_resource(UserLoginView, '/login')
